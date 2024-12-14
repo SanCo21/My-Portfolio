a7d85b11eba6a60c2f26747432b1539dd560e5f9
Sandra C. <sandra.colomer@outlook.fr>
2024-12-14 14:23:28 +0100

    Improve the English/French translation in HomePage.

--- src/app/page.js
+++ src/app/page.js
@@ -1,90 +1,71 @@
 "use client"
+
 import { useEffect, useState, useCallback } from 'react';
 import Header from './components/Header';
+import { contentEN, contentFR } from '../content';
 
 const HomePage = () => {
   const [lang, setLang] = useState('fr');
-  const [content, setContent] = useState({
-    title: 'Bienvenue sur Mon Portfolio !',
-        intro: 'Je suis Sandra COLOMER, une intégratrice web passionnée par la création de sites web dynamiques et interactifs.',
-        about: 'Voici une brève introduction à mon sujet...',
-        activities: 'Voici quelques activités que j\'appécie...',
-        projects: 'Voici quelques projets sur lesquels j\'ai travaillé...',
-        skills : 'Voici quelques compétences que je possède...',
-        contact: 'N\'hésitez pas à me contacter...'
-  });
-
-  const updateContent = useCallback((language) => {
-    setLang(language);
-    if (language === 'fr') {
-      setContent({
-        title: 'Bienvenue sur Mon Portfolio !',
-        intro: 'Je suis Sandra COLOMER, une intégratrice web passionnée par la création de sites web dynamiques et interactifs.',
-        about: 'Voici une brève introduction à mon sujet...',
-        activities: 'Voici quelques activités que j\'appécie...',
-        projects: 'Voici quelques projets sur lesquels j\'ai travaillé...',
-        skills : 'Voici quelques compétences que je possède...',
-        contact: 'N\'hésitez pas à me contacter...'
-      });
-    } else {
-      setContent({
-        title: 'Welcome to My Portfolio!',
-        intro: 'I am Sandra COLOMER, a web integrator passionate about creating dynamic and interactive websites.',
-        about: 'Here is a brief introduction about myself...',
-        activities: 'Here are some of the activities I enjoy doing...',
-        projects: 'Some of the projects I have worked on include...',
-        skills : 'Here are some of my skills...',
-        contact: 'Feel free to get in touch with me...'
-      });
-    }
-  }, []);
+  const [content, setContent] = useState(contentFR);
 
   useEffect(() => {
-    const storedLang = localStorage.getItem('language');
-    if (storedLang) {
-      updateContent(storedLang);
+    if (typeof window === 'undefined') {
+      const storedLang = localStorage.getItem('language') || 'fr';
+      setLang(storedLang);
+      setContent(storedLang === 'fr' ? contentFR : contentEN);
     }
-  }, [updateContent]);
+  }, []);
+    
+  const updateContent = useCallback((language) => {
+    setLang(language);
+    const newContent = language === 'fr' ? contentFR : contentEN;
+    setContent(newContent);
+  }, []);
 
-  const toggleLanguage = () => { 
-    const newLang = lang === 'fr' ? 'en' : 'fr'; 
-    updateContent(newLang); localStorage.setItem('language', newLang); 
+  const toggleLanguage = () => {
+    const newLang = lang === 'en' ? 'fr' : 'en';
+    updateContent(newLang);
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('language', newLang);
+    }
   };
 
-  return ( 
-  <div> 
-    <Header lang={lang} toggleLanguage={toggleLanguage} /> 
-    <main className="min-h-screen flex flex-col items-center justify-center mt-20"> 
-      <section id="home" className="section"> 
-        <h1 className="text-4xl font-bold text-blue-900">{content.title}</h1> 
-        <p className="text-lg mt-4 text-blue-900">{content.intro}</p> 
-        
-      </section> <section id="about" className="section mt-8"> 
-        <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'À Propos de Moi' : 'About Me'}</h2> 
-        <p className="text-lg mt-4 text-blue-900">{content.about}</p> 
-      </section> 
+  return (
+    <div>
+      <Header lang={lang} toggleLanguage={toggleLanguage} />
+      <main className="min-h-screen flex flex-col items-start justify-center mt-20 ml-12">
+        <section id="home" className="section">
+          <h1 className="text-4xl font-bold text-blue-900">{content.title}</h1>
+          <p className="text-lg mt-4 text-blue-900">{content.intro}</p>
+        </section>
 
-      <section id="activities" className="section mt-8"> 
-        <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'Activités' : 'Activities'}</h2> 
-        <p className="text-lg mt-4 text-blue-900">{content.activities}</p> 
-      </section> 
+        <section id="about" className="section mt-8">
+          <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'À Propos de moi' : 'About me'}</h2>
+          <p className="text-lg mt-4 text-blue-900">{content.about}</p>
+        </section>
 
-      <section id="projects" className="section mt-8"> 
-        <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'Projets' : 'Projects'}</h2> 
-        <p className="text-lg mt-4 text-blue-900">{content.projects}</p> 
-      </section> 
-        
-      <section id="skills" className="section mt-8"> 
-        <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'Compétences' : 'Skills'}</h2> 
-        <p className="text-lg mt-4 text-blue-900">{content.skills}</p> 
-      </section> 
-        
-      <section id="contact" className="section mt-8"> 
-        <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'Contact' : 'Contact'}</h2> 
-        <p className="text-lg mt-4 text-blue-900">{content.contact}</p> 
-      </section> 
+        <section id="activities" className="section mt-8">
+          <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'Activités' : 'Activities'}</h2>
+          <p className="text-lg mt-4 text-blue-900">{content.activities}</p>
+        </section>
 
-    </main> 
-  </div> ); }; 
+        <section id="projects" className="section mt-8">
+          <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'Projets' : 'Projects'}</h2>
+          <p className="text-lg mt-4 text-blue-900">{content.projects}</p>
+        </section>
+
+        <section id="skills" className="section mt-8">
+          <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'Compétences' : 'Skills'}</h2>
+          <p className="text-lg mt-4 text-blue-900">{content.skills}</p>
+        </section>
+
+        <section id="contact" className="section mt-8">
+          <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'Contact' : 'Contact'}</h2>
+          <p className="text-lg mt-4 text-blue-900">{content.contact}</p>
+        </section>
+      </main>
+    </div>
+  );
+};
 
 export default HomePage;
---
+++ src/content.js
@@ -0,0 +1,21 @@
+
+export const contentEN = {
+    title: 'Welcome to My Portfolio!',
+    intro: 'I am Sandra COLOMER, a web integrator passionate about creating dynamic and interactive websites.',
+    about: 'Here is a brief introduction about myself...',
+    activities: 'Here are some of the activities I enjoy...',
+    projects: 'Some of the projects I have worked on include...',
+    skills: 'Here are some of my skills...',
+    contact: 'Feel free to get in touch with me...'
+};
+
+export const contentFR = {
+    title: 'Bienvenue sur Mon Portfolio !',
+    intro: 'Je suis Sandra COLOMER, une intégratrice web passionnée par la création de sites web dynamiques et interactifs.',
+    about: 'Voici une brève introduction à mon sujet...',
+    activities: 'Voici quelques activités que j\'apprécie...',
+    projects: 'Voici quelques projets sur lesquels j\'ai travaillé...',
+    skills: 'Voici quelques compétences que je possède...',
+    contact: 'N\'hésitez pas à me contacter pour plus d\'informations ou pour discuter de potentiels projets de collaboration.'
+};
+  
