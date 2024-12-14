8025945580389395df50c26d1332c9d7c99fd61c
Sandra C. <sandra.colomer@outlook.fr>
2024-12-14 18:47:24 +0100

    Refactor Header and HomePage components to use new content structure and improve language handling; remove deprecated content files.

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,22 +1,26 @@
 import Link from 'next/link';
 import LanguageSwitcher from './LanguageSwitcher';
+import sectionsData from '../../content';
 // import Image from 'next/image';
 
-const Header = ({ lang, toggleLanguage }) => {
+const Header = ({ lang, toggleLanguage, activeSection }) => {
+  const sections = sectionsData;
+
   return (
     <header className="bg-blue-900 text-white p-4 fixed top-0 left-0 w-full z-10">  
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center">
+        <div className="flex items-center flex-grow-0">
           {/* <Image src="/logo.png" alt="Logo" width={50} height={50} /> */}
-          <h1 className="text-xl font-bold ml-2">My Portfolio</h1>          
+          <h1 className="text-xl font-bold ml-0 mr-5">
+            {lang === 'fr' ? sections.home.title.fr : sections.home.title.en}
+          </h1>          
         </div>
         <nav className="flex space-x-4">
-          <Link href="#home" className="hover:text-yellow-500">Home</Link>          
-          <Link href="#about" className="hover:text-yellow-500">About</Link>
-          <Link href="#home" className="hover:text-yellow-500">Activities</Link>
-          <Link href="#projects" className="hover:text-yellow-500">Projects</Link>
-          <Link href="#projects" className="hover:text-yellow-500">Skills</Link>
-          <Link href="#contact" className="hover:text-yellow-500">Contact</Link>
+          {Object.keys(sections).filter(key => key !== 'home').map(key => ( 
+            <Link href={`#${sections[key].id}`} key={key} scroll={false} className={`hover:text-yellow-500 ${activeSection === sections[key].id ? 'bg-white text-blue-900' : ''} ${sections[key].id === 'about' ? 'whitespace-nowrap' : ''}`}> 
+              {lang === 'fr' ? sections[key].title.fr : sections[key].title.en} 
+            </Link> 
+          ))}
         </nav>
         <LanguageSwitcher lang={lang} toggleLanguage={toggleLanguage} />  
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -2,67 +2,77 @@
 
 import { useEffect, useState, useCallback } from 'react';
 import Header from './components/Header';
-import { contentEN, contentFR } from '../content';
+// import { contentEN, contentFR } from '../content';
+import sectionsData from '../content';
 
 const HomePage = () => {
   const [lang, setLang] = useState('fr');
-  const [content, setContent] = useState(contentFR);
+  const [activeSection, setActiveSection] = useState('home');  
+  // const sections = sectionsData;
 
   useEffect(() => {
-    if (typeof window === 'undefined') {
-      const storedLang = localStorage.getItem('language') || 'fr';
-      setLang(storedLang);
-      setContent(storedLang === 'fr' ? contentFR : contentEN);
-    }
-  }, []);
-    
-  const updateContent = useCallback((language) => {
-    setLang(language);
-    const newContent = language === 'fr' ? contentFR : contentEN;
-    setContent(newContent);
+    const storedLang = localStorage.getItem('language') || 'fr';
+    setLang(storedLang);
   }, []);
 
+  const handleScroll = useCallback(() => { 
+    const offsets = Object.keys(sectionsData).map(key => { 
+      const el = document.getElementById(sections[key].id);
+      if (el) { 
+        const offsetTop = el.offsetTop; 
+        // console.log(`Section: ${sections[key].id}, Offset: ${offsetTop}`);
+        return { section: sections[key].id, offset: offsetTop };  
+      }
+      return { section:sections[key].id, offset: 0 };    
+    });
+
+    const currentSection = offsets.reduce((acc, curr) => { 
+      if (window.scrollY >= curr.offset - 100) { // adjust to consider the height of the header 
+        return curr.section; 
+      } 
+      return acc; 
+    }, 'home'); 
+    
+    console.log(`Current Section: ${currentSection}`);
+    setActiveSection(currentSection); 
+  }, []); 
+  
+  useEffect(() => { 
+    window.addEventListener('scroll', handleScroll); 
+    return () => { 
+      window.removeEventListener('scroll', handleScroll); 
+    }; }, [handleScroll]);
+    
+    // const updateContent = useCallback((language) => {
+    //   setLang(language);
+    //   if (typeof window !== 'undefined') {
+    //     localStorage.setItem('language', language);
+    //   }
+    // }, []);
+  
   const toggleLanguage = () => {
     const newLang = lang === 'en' ? 'fr' : 'en';
-    updateContent(newLang);
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('language', newLang);
-    }
+    setLang(newLang);
+    localStorage.setItem('language', newLang);
   };
-
+  
   return (
     <div>
-      <Header lang={lang} toggleLanguage={toggleLanguage} />
-      <main className="min-h-screen flex flex-col items-start justify-center mt-20 ml-12">
-        <section id="home" className="section">
-          <h1 className="text-4xl font-bold text-blue-900">{content.title}</h1>
-          <p className="text-lg mt-4 text-blue-900">{content.intro}</p>
+      <Header lang={lang} toggleLanguage={toggleLanguage} activeSection={activeSection}/>
+      <main className="min-h-screen flex flex-col items-start justify-center mt-20 ml-12 mr-3">
+        {/* Intro Section */}
+        <section id="home" className="section mt-20">
+          <h1 className="text-4xl font-bold text-blue-900">{lang === 'fr' ? sectionsData.home.title.fr : sectionsData.home.title.en}</h1>
+          <p className="text-lg mt-4 text-blue-900">{lang === 'fr' ? sectionsData.home.intro.fr : sectionsData.home.intro.en}</p>
+          <p className="text-lg mt-4 text-blue-900">{lang === 'fr' ? sectionsData.home.description.fr : sectionsData.home.description.en}</p>
         </section>
-
-        <section id="about" className="section mt-8">
-          <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'À Propos de moi' : 'About me'}</h2>
-          <p className="text-lg mt-4 text-blue-900">{content.about}</p>
-        </section>
-
-        <section id="activities" className="section mt-8">
-          <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'Activités' : 'Activities'}</h2>
-          <p className="text-lg mt-4 text-blue-900">{content.activities}</p>
-        </section>
-
-        <section id="projects" className="section mt-8">
-          <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'Projets' : 'Projects'}</h2>
-          <p className="text-lg mt-4 text-blue-900">{content.projects}</p>
-        </section>
-
-        <section id="skills" className="section mt-8">
-          <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'Compétences' : 'Skills'}</h2>
-          <p className="text-lg mt-4 text-blue-900">{content.skills}</p>
-        </section>
-
-        <section id="contact" className="section mt-8">
-          <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? 'Contact' : 'Contact'}</h2>
-          <p className="text-lg mt-4 text-blue-900">{content.contact}</p>
-        </section>
+        {/* Other Sections */}
+        {Object.keys(sectionsData).filter(key => key !== 'home').map(key => ( 
+          <section id={sectionsData[key].id} className="section mt-20" key={key}> 
+            <h2 className="text-3xl font-bold text-blue-900">{lang === 'fr' ? sectionsData[key].title.fr : sectionsData[key].title.en}</h2> 
+            <p className="text-lg mt-4 text-blue-900">{lang === 'fr' ? sectionsData[key].content.fr : sectionsData[key].content.en}</p> 
+          </section> 
+        ))}
       </main>
     </div>
   );
