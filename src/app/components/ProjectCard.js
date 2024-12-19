a2eef0029197476879e5b6bd5cfbf01256c275f2
Sandra C. <sandra.colomer@outlook.fr>
2024-12-19 16:55:55 +0100

    add projects

--- src/app/components/ProjectCard.js
+++ src/app/components/ProjectCard.js
@@ -1,18 +1,26 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 const ProjectCards = ({ lang, projects }) => {
+
+  const [mounted, setMounted] = useState(false); 
+  useEffect(() => { 
+    setMounted(true); 
+  }, []); 
+  if (!mounted) return null;
+
   return (
     <div className="projects-content mt-10 flex flex-wrap justify-center">
       {projects.map((project, index) => (
         <div key={index} className="m-4 p-4 bg-white shadow-lg rounded-lg w-80">
           <Image className="w-full h-60 object-cover rounded-lg"
             src={project.imageUrl}
-            alt={project.title[lang]}
-            width={300}
-            height={300}            
+            alt={project.title}
+            width={900}
+            height={900}            
           />
-          <h3 className="mt-4 text-xl font-bold">{project.title[lang]}</h3>
-          <p className="mt-2 text-blue-900">{project.description[lang]}</p>
+          <h3 className="mt-4 text-xl font-bold">{project.title}</h3>
+          <p className="mt-2 mb-2 text-blue-900">{project.description[lang]}</p>
           <a className="mt-4 text-yellow-500 hover:underline"
             href={project.link}
             target="_blank"
---
+++ src/app/pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="fr">
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+          <meta name="description" content="Bienvenue sur le portfolio de Sandra COLOMER. Explorez mes projets de développement web, apprenez-en davantage sur moi, et contactez-moi pour vos projets numériques." />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
